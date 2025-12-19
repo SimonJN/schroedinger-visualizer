8308f9467d2c82b72558218133f7d4383051bff9
Järn Simon <simon.jarn@aalto.fi>
2025-12-19 16:38:43 +0200

    Add functionality for finding all possible eigenfunctions and -values of the wavefunctions

--- src/main.cpp
+++ src/main.cpp
@@ -53,12 +53,13 @@ NumericalFunction rungekutta(double y0, double h, uint max_n) {
     
     return constructed_function;
 }
+double param = 1;
 
 double funcSecond(double x, double y0, double y1) {
     double hbar = 1.054571817e-34; // Js
     double m_e = 9.1093837139e-31; // kg
 
-    return -1.577e8*y0; // kg*J/(Js)^2
+    return -param*y0; // kg*J/(Js)^2
 }
 
 NumericalFunction rungekuttanystrom() {
@@ -67,7 +68,7 @@ NumericalFunction rungekuttanystrom() {
     double hbar = 1.054571817e-34; // Js
     double m_e = 9.1093837139e-31; // kg
 
-    double h = 0.0001e-3; // m
+    double h = 0.00001e-3; // m
 
     double y0_n = 0; // unitless
     double y1_n = 1; // 1/m
@@ -77,9 +78,9 @@ NumericalFunction rungekuttanystrom() {
     constructed_function.x.push_back(x_n);
     constructed_function.y.push_back(y0_n);
 
-    std::cout << x_n << "\t" << y0_n << std::endl;
+    //std::cout << x_n << "\t" << y0_n << std::endl;
 
-    for (size_t n = 1; n < 10001; n++)
+    for (size_t n = 1; n < 100001; n++)
     {
         double k1 = funcSecond(x_n, y0_n, y1_n);
 
@@ -104,7 +105,7 @@ NumericalFunction rungekuttanystrom() {
         constructed_function.x.push_back(x_n);
         constructed_function.y.push_back(y0_n);
 
-        std::cout << x_n << "\t" << y0_n << std::endl;
+        //std::cout << x_n << "\t" << y0_n << std::endl;
     }
     
     return constructed_function;
@@ -127,14 +128,114 @@ NumericalFunction normalizeWavefunction(NumericalFunction input) {
         output.x.push_back(input.x[i]);
         output.y.push_back(input.y[i]/correction_factor);
     }
-    std::cout << output.y[input.x.size()-1] << std::endl;
+    //std::cout << output.y[input.x.size()-1] << std::endl;
     return output;
 }
 
+int numRoots(NumericalFunction input) {
+    int zeros = 0;
+    for (size_t i = 1; i < input.x.size(); i++)
+    {
+        if (input.y[i]*input.y[i-1] < 0)
+        {
+            zeros += 1;
+        }
+    }
+    return zeros;    
+}
+
 int main() {
-    NumericalFunction result = rungekuttanystrom();
-    result = normalizeWavefunction(result);
-    writeCSV(result, "numerical.csv");
+    NumericalFunction params;
+
+    for (size_t i = 1; i < 10; i++)
+    {
+        NumericalFunction result;
+        result = rungekuttanystrom();
+        result = normalizeWavefunction(result);
+
+        params.x.push_back(param);
+        params.y.push_back(result.y[result.y.size() - 1]);
+
+        NumericalFunction as;
+        as.x.push_back(param);
+        as.y.push_back(result.y[result.y.size() - 1]);
+
+        NumericalFunction bs;
+
+        while (bs.x.size() < 1)
+        {
+            int roots = numRoots(result);
+            if (roots >= i)
+            {
+                bs.x.push_back(param);
+                bs.y.push_back(result.y[result.y.size() - 1]);
+            } else
+            {
+                as.x.push_back(param);
+                as.y.push_back(result.y[result.y.size() - 1]);
+
+                param = param * 2;
+                result = rungekuttanystrom();
+                result = normalizeWavefunction(result);
+            }
+        }
+        //std::cout << "Found new bounds: " << as.x[as.x.size() - 1] << "," << bs.x[bs.x.size() - 1] << std::endl;
+        // Ensure only one root
+        int roots = -1;
+        while (roots != i)
+        {
+            double middle = (bs.x[bs.x.size() - 1] + as.x[as.x.size() - 1]) / 2;
+            param = middle;
+
+            result = rungekuttanystrom();
+            result = normalizeWavefunction(result); 
+
+            params.x.push_back(param);
+            params.y.push_back(result.y[result.y.size() - 1]);
+
+            roots = numRoots(result);
+            if (roots >= i)
+            {
+                bs.x.push_back(param);
+                bs.y.push_back(result.y[result.y.size() - 1]);
+            } else if (roots < i)
+            {
+                as.x.push_back(param);
+                as.y.push_back(result.y[result.y.size() - 1]);
+            }
+        }
+        
+        // Binary search
+        while (std::abs(result.y[result.y.size() - 1]) > 0.00001)
+        {
+            double middle = (bs.x[bs.x.size() - 1] + as.x[as.x.size() - 1]) / 2;
+            param = middle;
+
+            result = rungekuttanystrom();
+            result = normalizeWavefunction(result); 
+
+            params.x.push_back(param);
+            params.y.push_back(result.y[result.y.size() - 1]);
+            
+            if (result.y[result.y.size() - 1]*as.y[0] < 0)
+            {
+                bs.x.push_back(param);
+                bs.y.push_back(result.y[result.y.size() - 1]);
+            } else
+            {
+                as.x.push_back(param);
+                as.y.push_back(result.y[result.y.size() - 1]);
+            }
+        }
+        
+        // result = rungekuttanystrom();
+        // result = normalizeWavefunction(result);
+        writeCSV(result, "numerical.csv");
+        std::cout << "Eigenstate: " << i << ", Eigenvalue: " << param << std::endl;
+        param += 10000;
+    }
+
+    writeCSV(params, "params.csv");
 
     return 0;
 }
--- visualize.py
+++ visualize.py
@@ -21,7 +21,28 @@ analytic_y = []
 for xs in analytic_x:
     analytic_y.append(math.exp(0.5*(xs-0.5*math.sin(2*xs))+math.log(y0)))
 
-plt.plot(x,y, "x")
+plt.plot(x,y, "-")
 plt.plot(x,y2)
+plt.xlabel("Distance (m)")
+plt.ylabel("Value")
+plt.legend(["Wavefunction", "Probability"])
+#plt.plot(analytic_x, analytic_y)
+plt.show()
+
+# Params
+params = []
+errors = []
+
+with open('build/params.csv', mode ='r')as file:
+    csv_file = csv.reader(file)
+
+    for line in csv_file:
+        params.append(float(line[0]))
+        errors.append(float(line[1]))
+
+plt.plot(params, errors, "x-")
+plt.xlabel("Param")
+plt.ylabel("Error")
+plt.legend(["Error"])
 #plt.plot(analytic_x, analytic_y)
 plt.show()
